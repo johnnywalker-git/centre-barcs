582dc6e2d26b9bbdcb485ca7ffd8a5442e240b27
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-02-27 18:28:49 +0000

    Completed mobile menu functionality.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let menuButton  = document.querySelector(".menu-mobile")
-let logoImage  = document.querySelector(".logo-img")
+let logoImage  = document.querySelector(".logo-img-mob")
 let navList = document.querySelector(".nav-button-list")
 let navContainer = document.querySelector(".nav-container")
 let navContainerMobile = document.querySelector(".nav-container-mobile")
 
 
+
 function openMenu() {
   navContainer.style.display = "none"
-  navContainer.style.display = "flex"
+  navContainerMobile.style.display = "flex"
+
   console.log("Mobile viewing initiated.")
 }
+
+function closeMenu() {
+    navContainer.style.display = "flex"
+    navContainerMobile.style.display = "none"
+      
+    console.log(" viewing initiated.")
+  }
