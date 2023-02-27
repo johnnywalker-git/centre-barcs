3412bf76cb3b8a81db81e8c240e74548ea35ae73
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-02-27 17:32:47 +0000

    Enhancing mobile experience

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function navBarToggle() {
-    console.log("HI")
+let menuButton  = document.querySelector(".menu-mobile")
+let logoImage  = document.querySelector(".logo-img")
+let navList = document.querySelector(".nav-button-list")
+let navContainer = document.querySelector(".nav-container")
+let navContainerMobile = document.querySelector(".nav-container-mobile")
+
+
+function openMenu() {
+  navContainer.style.display = "none"
+  navContainer.style.display = "flex"
+  console.log("Mobile viewing initiated.")
 }
