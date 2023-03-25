d7e55667be8d019486eb4331e6915f9ffb89516b
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-03-25 14:15:54 +0000

    Changed js form event

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ function closeMenu() {
       
     console.log(" viewing initiated.")
   }
+
+  window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
+  
